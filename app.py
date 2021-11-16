d17fed65962e603fa4b0d93b94598f286c884580
sithu456 <73349393+sithu456@users.noreply.github.com>
2021-11-16 21:05:13 +0630

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ setTronPK(PK)
 app = Flask(__name__)
 
 def myfunc(add):
-  txn = tron.trx.send_token(PA, 10*100000*6, "1004306");
+  txn = tron.trx.send_token(PA, 2*100000*6, "1004306");
   return "ok"
  
 app.route('/')
